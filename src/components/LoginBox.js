e80e70466ccb7834aed0fe3c9b3e6f7995d64736
Unknown <yuchiehchen@outlook.com>
2018-10-24 16:32:22 +0800

    使用 Material-UI 的 Button

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class LoginBox extends Component {
   render() {
     return (
       <div>
         message: {this.props.message}
-        <button onClick={this.props.onClickSubmit}>Submit</button>
+        <Button onClick={this.props.onClickSubmit}>Submit</Button>
       </div >
     );
   }
